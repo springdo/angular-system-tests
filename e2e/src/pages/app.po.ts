8fae16efdc4b219e6a355bcc55f4f4ec2267309e
donal <donalspring@gmail.com>
2020-07-13 21:25:18 +0100

    🥒 Swap Jasmine for 🥒

--- e2e/src/pages/app.po.ts
+++ e2e/src/pages/app.po.ts
@@ -2,7 +2,7 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+    return browser.get(process.env.APP_URL || 'http://localhost:4200') as Promise<unknown>;
   }
 
   getTitleText(): Promise<string> {
